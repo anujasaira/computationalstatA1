de1744073661cd78fe8bae0430674e053289a9bd
Miiiriiiam00 <miriammercuri.mm@gmail.com>
2023-05-24 17:53:32 +0200

    boot and jstar

--- Exercise1_Miriam.R
+++ Exercise1_Miriam.R
@@ -11,32 +11,58 @@ abline(0,1,col = "red")
 
 set.seed(12345)
 
-B <- 10000
+B <- 1000
 
-bootMatrices <- array(0, dim = c(ncol(games), ncol(games),B))
-
-for (i in 1:B) {
+boot <- function(B, games){
   
-  bootSample <- games[sample(nrow(games),replace = TRUE),]
-  
-  bootCov <- cov(bootSample)
-  
-  bootMatrices[, , i] <- bootCov 
+  results <- matrix(NA, nrow = B, ncol = ncol(games)^2)
+  bootstrap <- replicate(B, {
+    
+    bootSample <- games[sample(nrow(games), replace = T),]
+    bootCov <- cov(bootSample)
+    i <- seq_len(B)
+    results[i,] <- as.vector(bootCov)
+  })
+  return(bootstrap)
 }
 
-standard_errors <- apply(bootMatrices, c(1,2), sd)
+boot <- boot(1000,games)
 
-means <- apply(bootMatrices, c(1,2), mean)
+standard_errors <- apply(boot, 1, sd)
+
+means <- apply(boot, 1, mean)
 
 
-qnt_CI <- quantile(bootMatrices, probs = c(0.025,0.975))
 
-lower_CLT <- means - 1.96 *standard_errors
-upper_CLT <- means + 1.96 *standard_errors
+qnt_CI <- t(apply(boot, 1, function(z) quantile(z, probs = c(0.025,0.975)))
+)
 
 clt_CI <- means + c(-1,1)*1.96*standard_errors
 
 rbind(clt_CI, qnt_CI)
+
+plot_results <- function(clt_CI, qnt_CI) {
+  n <- length(clt_CI)
+  plot(1:n, clt_CI[, 1], type = "l", ylim = range(c(clt_CI, qnt_CI)), 
+       xlab = "Entry", ylab = "Confidence Interval", main = "Bootstrap Confidence Intervals")
+  lines(1:n, clt_CI[, 2], type = "l", col = "blue")
+  lines(1:n, qnt_CI[, 1], type = "l", col = "green")
+  lines(1:n, qnt_CI[, 2], type = "l", col = "purple")
+  legend("topright", legend = c("CLT Lower", "CLT Upper", "Quantile Lower", "Quantile Upper"),
+         col = c("blue", "blue", "green", "purple"), lty = c(1, 1, 1, 1))
+}
+
+
+eigen_values <- eigen(cov_matrix)$values
+cumulative_proportion <- cumsum(eigen_values) / sum(eigen_values)
+
+jstar <- min(which(cumulative_proportion > 0.76))
+
+
+
+
+
+ 
 
 
 
