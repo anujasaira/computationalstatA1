a41a1c1c952f279106a56684fed405e621b887e8
Miiiriiiam00 <miriammercuri.mm@gmail.com>
2023-05-24 16:11:25 +0200

    CI

--- Exercise1_Miriam.R
+++ Exercise1_Miriam.R
@@ -9,6 +9,7 @@ plot(cov_matrix, cor_matrix,
      ylab = "Correlation Matrix")
 abline(0,1,col = "red")
 
+set.seed(12345)
 
 B <- 10000
 
@@ -25,8 +26,17 @@ for (i in 1:B) {
 
 standard_errors <- apply(bootMatrices, c(1,2), sd)
 
+means <- apply(bootMatrices, c(1,2), mean)
 
 
+qnt_CI <- quantile(bootMatrices, probs = c(0.025,0.975))
+
+lower_CLT <- means - 1.96 *standard_errors
+upper_CLT <- means + 1.96 *standard_errors
+
+clt_CI <- means + c(-1,1)*1.96*standard_errors
+
+rbind(clt_CI, qnt_CI)
 
 
 
